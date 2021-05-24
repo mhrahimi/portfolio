27905adff98267645e30d95b7e3781d89e7d15a7
mhrahimi <mamsrahimi@gmail.com>
2021-05-24 04:11:29 -0700

    1. First Simple Theme

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div>
+<h1><?php the_title(); ?></h1>
+<h4>Posted on <?php the_time('F jS, Y') ?></h4>
+<p><?php the_content(__('(more...)')); ?></p>
+</div>
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
